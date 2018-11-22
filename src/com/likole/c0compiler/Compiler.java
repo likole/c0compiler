488e4c50748445242bbcc963595f482d1a2df7f5
likole <likolef@gmail.com>
2018-11-22 16:59:32 +0800

    = =

--- src/com/likole/c0compiler/Compiler.java
+++ src/com/likole/c0compiler/Compiler.java
@@ -1,10 +1,21 @@
 package com.likole.c0compiler;
 
+import com.likole.c0compiler.compiler.utils.SymbolTable;
+import com.likole.c0compiler.interpreter.Interpreter;
+
 /**
  * @author kanghao
  * @date 18-11-22 下午3:16
  */
 public class Compiler {
+
+    SymbolTable symbolTable;
+    Interpreter interpreter;
+
+    public Compiler(){
+
+    }
+
 
     public static void main(String[] args) {
 
---
+++ src/com/likole/c0compiler/interpreter/impl/InterpreterImpl.java
@@ -0,0 +1,23 @@
+package com.likole.c0compiler.interpreter.impl;
+
+import com.likole.c0compiler.interpreter.Interpreter;
+
+/**
+ * Created by likole on 11/22/18.
+ */
+public class InterpreterImpl implements Interpreter {
+    @Override
+    public void listCode(int start) {
+
+    }
+
+    @Override
+    public void interpret() {
+
+    }
+
+    @Override
+    public int base(int l, int[] s, int b) {
+        return 0;
+    }
+}
