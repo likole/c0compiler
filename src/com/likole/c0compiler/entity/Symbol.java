4dc7c777ce5b63f07522d6b6d266e55ac52f0861
zhangkanghao <zhangkanghao@imudges.com>
2018-11-29 16:12:11 +0800

    实现词法分析,符号集加lss,leq,gtr,geq

--- src/com/likole/c0compiler/entity/Symbol.java
+++ src/com/likole/c0compiler/entity/Symbol.java
@@ -4,7 +4,7 @@ package com.likole.c0compiler.entity;
  *　各种符号的编码
  */
 public enum Symbol {
-    nul, become, ident, number, plus, minus, times, slash, eql, neq, lparen, rparen,
+    nul, becomes, ident, number, plus, minus, times, slash, eql, neq, lss, leq, gtr, geq, lparen, rparen,
     comma, semicolon, period,
     breaksym,casesym,constsym,continuesym,defaultsym,dosym,
     elsesym,forsym,ifsym,intsym,mainsym,switchsym,voidsym,whilesym,
